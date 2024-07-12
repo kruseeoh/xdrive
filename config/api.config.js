7d14cad96ee06ccc27af3f41960ae7b8050ddf5e
kruseeoh <116453803+kruseeoh@users.noreply.github.com>
2024-07-13 00:23:16 +0530

    meow
    meow

---
+++ config/api.config.js
@@ -0,0 +1,35 @@
+/**
+ * This file contains the configuration for the API endpoints and tokens we use.
+ *
+ * - If you are a OneDrive International user, you would not have to change anything here.
+ * - If you are not the admin of your OneDrive for Business account, you may need to define your own clientId/clientSecret,
+ *   check documentation for more details.
+ * - If you are using a E5 Subscription OneDrive for Business account, the direct links of your files are not the same here.
+ *   In which case you would need to change directLinkRegex.
+ */
+module.exports = {
+  // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
+  // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
+  clientId: '7d536b78-5d16-4490-828e-74f1e5ce4f70',
+  obfuscatedClientSecret: 'U2FsdGVkX18EWR5Q8HI8PdMMKg5QaE1RssesgA9X3XL14/nP7d61+SGcFz9rY2ARXr0CAPGi73fVgGLTpVSjvw==',
+
+  // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
+  // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
+  redirectUri: 'http://localhost',
+
+  // These are the URLs of the OneDrive API endpoints. You would not need to change anything here if you are using OneDrive International
+  // or E5 Subscription OneDrive for Business. You may need to change these if you are using OneDrive 世纪互联.
+  authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+  driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
+
+  // The scope we require are listed here, in most cases you would not need to change this as well.
+  scope: 'user.read files.read.all offline_access',
+  
+
+  // Cache-Control header, check Vercel documentation for more details. The default settings imply:
+  // - max-age=0: no cache for your browser
+  // - s-maxage=0: cache is fresh for 60 seconds on the edge, after which it becomes stale
+  // - stale-while-revalidate: allow serving stale content while revalidating on the edge
+  // https://vercel.com/docs/concepts/edge-network/caching
+  cacheControlHeader: 'max-age=0, s-maxage=60, stale-while-revalidate',
+}
